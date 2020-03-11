c2a9286e620cd6bca14d220904fefb61bbc62561
Lidor Avitan <lidoravitan@Lidors-MacBook-Pro.local>
2020-03-11 13:48:36 +0200

    api

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(toucheatapp.com www.toucheatapp.com)
+domains=(toucheatapp.com www.toucheatapp.com api.toucheatapp.com www.api.toucheatapp.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="tv.lidor@gmail.com" # Adding a valid address is strongly recommended
